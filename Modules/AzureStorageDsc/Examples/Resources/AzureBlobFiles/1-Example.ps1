5adafdc2e5bdffe57d95867f61fa99dc33471455
Brian Farnhill <brfarn@microsoft.com>
2016-11-06 16:02:35 +1100

    Fixed example text

--- Modules/AzureStorageDsc/Examples/Resources/AzureBlobFiles/1-Example.ps1
+++ Modules/AzureStorageDsc/Examples/Resources/AzureBlobFiles/1-Example.ps1
@@ -1,11 +1,22 @@
-Configuration BlobStorageSample
+<#
+.EXAMPLE
+    This example shows how to download the contents of a storage container
+    named 'container1' in a storage account called 'myfakeaccount' to
+    "C:\sample path"
+#>
+
+Configuration Example
 {
     Import-DscResource -ModuleName AzureStorageDsc
 
-    AzureBlobFiles ExampleFiles {
-        Path                    = "C:\sample path"
-        StorageAccountName      = "myfakeaccount"
-        StorageAccountContainer = "container1"
-        StorageAccountKey       = "Your Storage Key Goes Here"
+    node localhost
+    {
+        AzureBlobFiles ExampleFiles 
+        {
+            Path                    = "C:\sample path"
+            StorageAccountName      = "myfakeaccount"
+            StorageAccountContainer = "container1"
+            StorageAccountKey       = "Your Storage Key Goes Here"
+        }
     }
 }
